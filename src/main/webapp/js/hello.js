ce6fc0f11c3e6034c6a6344875b924fdb5b4bbe4
unknown <078825@SH-078825.deppon.com>
2014-04-12 08:45:50 +0800

    modify index.jsp at Apr 12

--- src/main/webapp/js/hello.js
+++ src/main/webapp/js/hello.js
@@ -13,9 +13,9 @@ Ext.onReady(function(){
 	Ext.Msg.alert("age",person.age);
 	Ext.Array.each(person.books,function(book,index){
 		Ext.Msg.alert("person info",person.name + person.age + book.name + book.price);
-	});*/
+	});
 	
-	/*var msg = {
+	var msg = {
 		title: 'Address',
 	    msg: 'Please enter your address:',
 	    width: 300,
@@ -47,23 +47,187 @@ Ext.onReady(function(){
 		},
 		interval:100
 	};
-	Ext.TaskManager.start(task);*/
+	Ext.TaskManager.start(task);
 	
-	/*Ext.Msg.prompt("title",'<font color=red>hello</font>',showid,this,true,"hi");
+	Ext.Msg.prompt("title",'<font color=red>hello</font>',showid,this,true,"hi");
 	function showid(id,msg) {
 		alert(id+":"+msg);
-	}*/
+	}
 	
 	//Ext.Msg.wait('please wait for a while','title',{text:'saving...'});
 	
-	var progressBar = new Ext.ProgressBar({
-		width:300,
-		renderTo:'progressBar'
+	var p = Ext.create('Ext.ProgressBar', {
+	   renderTo: Ext.getBody(),
+	   width: 300
 	});
-	progressBar.wait({
-		duration:10000,
-		interval:1000,
-		increment:10
+
+	p.wait({
+	    interval: 500, //bar will move fast!
+	    duration: 5000,
+	    increment: 10,
+	    text: 'Updating...',
+	    scope: this,
+	    fn: function(){
+	        p.updateText('Done!');
+	    }
+	});
+	var enableBtn = Ext.create('Ext.Button', {
+	    text: 'Enable',
+	    renderTo: Ext.getBody(),
+	    handler: function() {
+	    	//disableBtn.disable();
+	    	enableBtn.enable();
+	    	text.enable();
+	    	//toolbar.enable();
+	    }
 	});
 	
+	var disableBtn = Ext.create('Ext.Button', {
+	    text: 'Disable',
+	    renderTo: Ext.getBody(),
+	    handler: function() {
+	    	//enableBtn.disable();
+	    	disableBtn.enable();
+	    	text.disable();
+	    	//toolbar.disable();
+	    }
+	});
+	
+	var text = Ext.create('Ext.form.field.Text',{
+		 xtype: 'textfield',
+        name: 'name',
+        fieldLabel: 'Name',
+        allowBlank: false 
+	});
+	
+	var toolbar = Ext.create('Ext.toolbar.Toolbar', {
+	    renderTo: document.body,
+	    width   : 400,
+	    margin  : '5 0 0 0',
+	    items   : [enableBtn, disableBtn,text,menu]
+	});
+	
+	
+	var createMenu = Ext.create('Ext.menu.Menu', {
+	    width: 800,
+	    margin: '0 0 10 0',
+	    floating: false,  // usually you want this set to True (default)
+	    renderTo: Ext.getBody(),  // usually rendered by it's containing component
+	    items: [{
+	        		text: 'create'
+	        	},{
+	        		text:'save'
+	        	}
+        	]
+	});
+	
+	var editMenu = Ext.create('Ext.menu.Menu', {
+	    width: 800,
+	    margin: '0 0 10 0',
+	    floating: false,  // usually you want this set to True (default)
+	    renderTo: Ext.getBody(),  // usually rendered by it's containing component
+	    items: [{
+	        		text: 'modify'
+	        	}
+        	]
+	});
+	
+	var toolbar = Ext.create('Ext.toolbar.Toolbar',{
+		renderTo: document.body,
+	    width   : 400,
+	    margin  : '5 0 0 0',
+	    items   : [{text:'file',menu:createMenu},
+	    			{text:'edit',menu:editMenu}]
+	});*/
+	
+	Ext.create('Ext.form.Panel',{
+		title:'Info',
+		width:800,
+		items:[
+		{
+			xtype:'textfield',
+			name:'name',
+			fieldLabel:'Your name',
+			allowblank:false,
+			blankText:'not allow blank',
+			msgTarget:'under'
+		},{
+			xtype:'textfield',
+			name:'number',
+			fieldLabel:'Your Number',
+			inputType:'password',
+			allowblank:false
+		},{
+			xtype:'radiogroup',
+			
+			fieldLabel:'Sex',
+			items:[{
+				boxLabel:'Men',
+				name:'sex',
+				inputValue:'M'
+			},{
+				boxLabel:'Women',
+				name:'sex',
+				inputValue:'W'
+			}
+			]
+			
+		},{
+			xtype:'checkboxgroup',
+			
+			fieldLabel:'Books',
+			items:[{
+				boxLabel:'Spring',
+				name:'books',
+			inputValue:'SPRING'
+			},{
+				boxLabel:'Unix',
+				name:'books',
+			inputValue:'UNIX'
+			}
+			]
+			
+		},{
+			xtype:'triggerfield',
+			fieldLabel:'BeCool',
+			emptyText:'click me',
+			onTriggerClick: function() {
+        		Ext.Msg.alert('Status', 'You clicked my trigger!');
+   			 }
+		},{
+			xtype: 'numberfield',
+	        anchor: '100%',
+	        name: 'score',
+	        fieldLabel: 'Score',
+	        allowDecimals:false,
+	        allowBlank:false,
+	        //labelWdith:10,
+	        //width:10,
+	        value: 0,
+	        maxValue: 100,
+	        minValue: 0
+		}
+		],
+		buttons:[{
+			xtype:'button',
+			text:'save',
+			handler:function(btn){
+				//Ext.Msg.alert('hi',btn.getText());
+				//Ext.Msg.alert('hi',this.up('form').getForm().findField('name').getValue()+ "-"+this.up('form').getForm().findField('number').getValue());
+				var array = this.up('form').getForm().findField('books');
+				Ext.each(array,function(value){
+					console.log(value);
+				});
+				Ext.Msg.alert('hi',this.up('form').getForm().findField('books').inputValue+ "-"+this.up('form').getForm().findField('number').getValue());
+			}
+		},{
+			xtype:'button',
+			text:'cancel',
+			handler:function(){
+				this.up('form').getForm().reset();
+			}
+		}],
+		renderTo:Ext.getBody()
+	});
 });
+
